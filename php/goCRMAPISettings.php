120dc03b058777897f3c5f47938ddf6fd0d4161e
root <root@webrtc.goautodial.com>
2016-07-12 13:14:02 -0400

    Updates from WebRTC server

--- php/goCRMAPISettings.php
+++ php/goCRMAPISettings.php
@@ -7,6 +7,7 @@ define ('responsetype', 'json');
 
 define ('gourl', 'http://webrtc.goautodial.com/goAPI');
 define ('goUser', 'admin');
-define ('goPass', 'G02x16');
+//define ('goPass', 'G02x16');
+define ('goPass', 'Yq48yHo2g0');
 define ('responsetype', 'json');
 ?>
