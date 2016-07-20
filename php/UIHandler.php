8bfcf9c631e0bd9e76fdefc9ffeee4036b9449fe
Noel Umandap <noelumandap@gmail.com>
2016-07-21 01:35:29 +0800

    adjusted campaign add form elements

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -4951,6 +4951,26 @@ error_reporting(E_ERROR | E_PARSE);
 		</div>
 		';
 	}
+
+	public function getCountryCodes(){
+		$url = gourl."/goCountryCode/goAPI.php"; #URL to GoAutoDial API. (required)
+		$postfields["goUser"] = goUser; #Username goes here. (required)
+		$postfields["goPass"] = goPass;
+		$postfields["goAction"] = "getAllCountryCodes"; #action performed by the [[API:Functions]]
+		$postfields["responsetype"] = responsetype; #json. (required)
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		$output = json_decode($data);
+
+		return $output;
+	}
 }
 
 ?>
