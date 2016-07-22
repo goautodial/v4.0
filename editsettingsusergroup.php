31190cfcb10b8f13d8a0acbfa84133a58abb4781
Alexander Abenoja <alexandero.oabenoja@gmail.com>
2016-07-22 22:32:22 +0800

    Updated edit usergroup and voicemail forms

--- editsettingsusergroup.php
+++ editsettingsusergroup.php
@@ -51,6 +51,11 @@ if (isset($_POST["usergroup_id"])) {
         <!-- Creamy App -->
         <script src="js/app.min.js" type="text/javascript"></script>
 
+        	<!-- =============== BOOTSTRAP STYLES ===============-->
+			<link rel="stylesheet" href="theme_dashboard/css/bootstrap.css" id="bscss">
+				<!-- =============== APP STYLES ===============-->
+			<link rel="stylesheet" href="theme_dashboard/css/app.css" id="maincss">
+
         <!-- preloader -->
         <link rel="stylesheet" href="css/customizedLoader.css">
 
@@ -76,7 +81,7 @@ if (isset($_POST["usergroup_id"])) {
             <aside class="right-side">
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
-                    <h1>
+                    <h1 style="font-weight:normal;">
                         <?php $lh->translateText("settings"); ?>
                         <small><?php $lh->translateText("User Group Edit"); ?></small>
                     </h1>
@@ -94,9 +99,6 @@ if (isset($_POST["usergroup_id"])) {
                     </ol>
                 </section>
 
-                <!-- Main content -->
-                <section class="content">
-					<div class="box box-info">
 						<!-- standard custom edition form -->
 					<?php
 					$errormessage = NULL;
@@ -125,138 +127,164 @@ if (isset($_POST["usergroup_id"])) {
 						# Result was OK!
 							for($i=0;$i<count($output->user_group);$i++){
 					?>
-                    
-                    <div class="box-header with-border">
-							<h3 class="box-title">MODIFY USER GROUP : <u><?php echo $usergroup_id;?></u></h3>
-						</div>
-						<div class="box-body table-responsive">		
+
+            <!-- Main content -->
+            <section class="content">
+				<div class="panel panel-default">
+                    <div class="panel-body">
+						<legend>MODIFY USER GROUP : <u><?php echo $usergroup_id;?></u></legend>
+                    	
 							<form id="modifyvoicemail">
 								<input type="hidden" name="modifyid" value="<?php echo $usergroup_id;?>">
 							
-						<!-- BASIC SETTINGS -->
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="group_name">Group Name</label>
-									<input type="text" class="form-control" name="group_name" id="group_name" placeholder="Group Name (Mandatory)" value="<?php echo $output->group_name[$i];?>">
-								</div>
-								<div class="form-group">
-									<label for="forced_timeclock_login">Force Timeclock Login
-									<select class="form-control" name="forced_timeclock_login" id="forced_timeclock_login">
-									<?php
-										$forced_timeclock_login = NULL;
-										if($output->forced_timeclock_login[$i] == "N"){
-											$forced_timeclock_login .= '<option value="N" selected> NO </option>';
-										}else{
-											$forced_timeclock_login .= '<option value="N" > NO </option>';
-										}
-										
-										if($output->forced_timeclock_login[$i] == "Y"){
-											$forced_timeclock_login .= '<option value="Y" selected> YES </option>';
-										}else{
-											$forced_timeclock_login .= '<option value="Y" > YES </option>';
-										}
+						<!-- Custom Tabs -->
+						<div role="tabpanel">
+						<!--<div class="nav-tabs-custom">-->
+							<ul role="tablist" class="nav nav-tabs">
+								<li class="active"><a href="#tab_1" data-toggle="tab"><em class="fa fa-gear fa-lg"></em> Basic Settings</a></li>
+							</ul>
+			               <!-- Tab panes-->
+			               <div class="tab-content">
 
-										if($output->forced_timeclock_login[$i] == "ADMIN_EXEMPT"){
-											$forced_timeclock_login .= '<option value="ADMIN_EXEMPT" selected> ADMIN EXEMPT </option>';
-										}else{
-											$forced_timeclock_login .= '<option value="ADMIN_EXEMPT" > ADMIN EXEMPT </option>';
-										}
-										echo $forced_timeclock_login;
-									?>
-									</select>
-									</label>
-								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="row" style="padding-top:5px;">
-									<label for="shift_enforcement" class="col-md-5">Shift Enforcement
-									<select class="form-control" id="shift_enforcement" name="shift_enforcement">
-										<?php
-											$shift_enforcement = NULL;
+				               	<!-- BASIC SETTINGS -->
+				                <div id="tab_1" class="tab-pane fade in active">
+				                	<fieldset>
+										<div class="form-group mt">
+											<label for="group_name" class="col-sm-2 control-label">Group Name</label>
+											<div class="col-sm-10 mb">
+												<input type="text" class="form-control" name="group_name" id="group_name" placeholder="Group Name (Mandatory)" value="<?php echo $output->group_name[$i];?>">
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="forced_timeclock_login" class="col-sm-2 control-label">Force Timeclock Login</label>
+											<div class="col-sm-10 mb">
+												<select class="form-control" name="forced_timeclock_login" id="forced_timeclock_login">
+												<?php
+													$forced_timeclock_login = NULL;
+													if($output->forced_timeclock_login[$i] == "N"){
+														$forced_timeclock_login .= '<option value="N" selected> NO </option>';
+													}else{
+														$forced_timeclock_login .= '<option value="N" > NO </option>';
+													}
+													
+													if($output->forced_timeclock_login[$i] == "Y"){
+														$forced_timeclock_login .= '<option value="Y" selected> YES </option>';
+													}else{
+														$forced_timeclock_login .= '<option value="Y" > YES </option>';
+													}
 
-											if($output->shift_enforcement[$i] == "OFF" || $output->shift_enforcement[$i] == ""){
-												$shift_enforcement .= '<option value="OFF" selected> OFF </option>';
-											}else{
-												$shift_enforcement .= '<option value="OFF" > OFF </option>';
-											}
-											
-											if($output->shift_enforcement[$i] == "START"){
-												$shift_enforcement .= '<option value="START" selected> START </option>';
-											}else{
-												$shift_enforcement .= '<option value="START" > START </option>';
-											}
-		                                    
-		                                    if($output->shift_enforcement[$i] == "ALL"){
-												$shift_enforcement .= '<option value="ALL" selected> ALL </option>';
-											}else{
-												$shift_enforcement .= '<option value="ALL" > ALL </option>';
-											}
-		                                    
-											echo $shift_enforcement;
-										?>
-									</select>
-									</label>
-								</div>
-								<div class="form-group" style="padding-top:5px;">
-									<label for="group_level">Group Level
-									<select class="form-control" name="group_level" id="group_level">
-									<?php
-										$group_level = NULL;
+													if($output->forced_timeclock_login[$i] == "ADMIN_EXEMPT"){
+														$forced_timeclock_login .= '<option value="ADMIN_EXEMPT" selected> ADMIN EXEMPT </option>';
+													}else{
+														$forced_timeclock_login .= '<option value="ADMIN_EXEMPT" > ADMIN EXEMPT </option>';
+													}
+													echo $forced_timeclock_login;
+												?>
+												</select>
+											</div>
+										</div>
+								
+										<div class="form-group">
+											<label for="shift_enforcement" class="col-sm-2 control-label">Shift Enforcement</label>
+											<div class="col-sm-10 mb">
+												<select class="form-control" id="shift_enforcement" name="shift_enforcement">
+													<?php
+														$shift_enforcement = NULL;
 
-										for($o=1; $o <= 9; $o++){
-											if($output->group_level[$i] == $o){
-												$group_level .= '<option value="'.$o.'" selected> '.$o.' </option>';
-											}else{
-												$group_level .= '<option value="'.$o.'"> '.$o.' </option>';
-											}
-										}
+														if($output->shift_enforcement[$i] == "OFF" || $output->shift_enforcement[$i] == ""){
+															$shift_enforcement .= '<option value="OFF" selected> OFF </option>';
+														}else{
+															$shift_enforcement .= '<option value="OFF" > OFF </option>';
+														}
+														
+														if($output->shift_enforcement[$i] == "START"){
+															$shift_enforcement .= '<option value="START" selected> START </option>';
+														}else{
+															$shift_enforcement .= '<option value="START" > START </option>';
+														}
+					                                    
+					                                    if($output->shift_enforcement[$i] == "ALL"){
+															$shift_enforcement .= '<option value="ALL" selected> ALL </option>';
+														}else{
+															$shift_enforcement .= '<option value="ALL" > ALL </option>';
+														}
+					                                    
+														echo $shift_enforcement;
+													?>
+												</select>
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="group_level" class="col-sm-2 control-label">Group Level</label>
+											<div class="col-sm-10 mb">
+												<select class="form-control" name="group_level" id="group_level">
+												<?php
+													$group_level = NULL;
 
-										echo $group_level;
-									?>
-									</select>
-									</label>
-								</div>
-								<br/>
+													for($o=1; $o <= 9; $o++){
+														if($output->group_level[$i] == $o){
+															$group_level .= '<option value="'.$o.'" selected> '.$o.' </option>';
+														}else{
+															$group_level .= '<option value="'.$o.'"> '.$o.' </option>';
+														}
+													}
 
-							</div>
-						</div>
-							<!-- NOTIFICATIONS -->
-		                    <div id="notifications">
-		                        <div class="output-message-success" style="display:none;">
-		                            <div class="alert alert-success alert-dismissible" role="alert">
-		                              <strong>Success!</strong> Phone <?php echo $usergroup_id?> modified !
-		                            </div>
-		                        </div>
-		                        <div class="output-message-error" style="display:none;">
-		                            <div class="alert alert-danger alert-dismissible" role="alert">
-		                              <span id="modifyT_phonesresult"></span>
-		                            </div>
-		                        </div>
-		                    </div>
+													echo $group_level;
+												?>
+												</select>
+											</div>
+										</div>
+									</fieldset>
+								</div><!-- tab 1 -->
 
-						<div class="box-footer">
-							<a type="button" class="btn btn-danger delete-phone" href="settingsusergroups.php"><i class="fa fa-arrow-left"></i> Cancel</a>
+								
+								<!-- NOTIFICATIONS -->
+			                    <div id="notifications">
+			                        <div class="output-message-success" style="display:none;">
+			                            <div class="alert alert-success alert-dismissible" role="alert">
+			                              <strong>Success!</strong> Phone <?php echo $usergroup_id?> modified !
+			                            </div>
+			                        </div>
+			                        <div class="output-message-error" style="display:none;">
+			                            <div class="alert alert-danger alert-dismissible" role="alert">
+			                              <span id="modifyT_phonesresult"></span>
+			                            </div>
+			                        </div>
+			                    </div>
 
-							<button type="submit" class="btn btn-primary pull-right" id="modifyUserGroupOkButton" href=""><span id="update_button"><i class="fa fa-check"></i> Update</span></button>
-						</div>
-							
-							</form>								
-							
-						</div>
+			                    <!-- FOOTER BUTTONS -->
+							   	<fieldset>
+			                        <div class="box-footer">
+			                           <div class="pull-right col-sm-3">
+												<a href="settingsusergroups.php" type="button" class="btn btn-danger pull-left"><i class="fa fa-close"></i> Cancel </a>
+			                           		
+			                                	<button type="submit" class="btn btn-primary pull-right" id="modifyUserGroupOkButton" href=""> <span id="update_button"><i class="fa fa-check"></i> Update</span></button>
+											</div>
+			                           </div>
+			                        </div>
+			                    </fieldset>
 
+				            	</div><!-- end of tab content -->
+	                    	</div><!-- tab panel -->
+	                    </form>
+	                </div><!-- body -->
+	            </div>
+            </section>
 					<?php
 							}
 						}	
                         
 					?>
-                </section>
+					
 				<!-- /.content -->
             </aside><!-- /.right-side -->
 			
-            <?php print $ui->creamyFooter(); ?>
-			
+			<?php print $ui->creamyFooter(); ?>
+
         </div><!-- ./wrapper -->
 
+  		
+		
 		<!-- Modal Dialogs -->
 		<?php include_once "./php/ModalPasswordDialogs.php" ?>
 		
