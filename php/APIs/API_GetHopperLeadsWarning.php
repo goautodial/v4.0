878bdac1d9b1f2a1c105313863fd72e384f00b91
Demian Biscocho <demian@goautodial.com>
2016-08-12 12:51:58 -0400

    Minor updates.

--- php/APIs/API_GetHopperLeadsWarning.php
+++ php/APIs/API_GetHopperLeadsWarning.php
@@ -8,16 +8,26 @@ $postfields["goAction"] = "goGetHopperLeadsWarning"; #action performed by the [[
 $postfields["responsetype"] = responsetype; 
 
 $ch = curl_init();
-     curl_setopt($ch, CURLOPT_URL, $url);
-     curl_setopt($ch, CURLOPT_POST, 1);
-     curl_setopt($ch, CURLOPT_TIMEOUT, 100);
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-     curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-     $data = curl_exec($ch);
-     curl_close($ch);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+$data = curl_exec($ch);
+curl_close($ch);
 
-     $output = json_decode($data);
-     
+$output = json_decode($data);
+
+if ($output == NULL){
+    echo '<small class="text-muted pull-right ml" style="padding-right:20px;"></small>
+                <div class="media-box-heading"><div class="media-box-heading"><strong>
+                <a href="#" class="text-danger m0">There is no available data.</a></strong></div>
+                </span>
+                <p class="m0">
+                <small ><strong><a href="#" class="text"></a></strong>
+                </small>
+                </p>';
+}    
 //milo
 for($i=0;$i < count($output->campaign_id);$i++){
     $campname = $output->campaign_name[$i];
@@ -25,23 +35,23 @@ for($i=0;$i < count($output->campaign_id);$i++){
     $campid =  $output->campaign_id[$i];
     
    if ($leadscount == 0){   
-        echo '<small class="text-muted pull-right ml">'.$leadscount.'</small>
-                <div class="media-box-heading"><div class="media-box-heading"><strong>
+        echo '<div class="media-box-heading"><div class="media-box-heading"><strong>
                 <a href="#" class="text-danger m0">'.$campname.'</a></strong></div>
                 </span>
                 <p class="m0">
                 <small ><strong><a href="#" class="text">'.$campid.'</a></strong>
                 </small>
+                <small class="text-muted pull-right ml" style="padding-right:20px;">'.$leadscount.'</small>
                 </p>';
     }
     if ($leadscount > 0){ 
-        echo '<small class="text-muted pull-right ml">'.$leadscount.'</small>
-                <div class="media-box-heading"><div class="media-box-heading"><strong>
+        echo '<div class="media-box-heading"><div class="media-box-heading"><strong>
                 <a href="#" class="text-warning m0">'.$campname.'</a></strong></div>
                 </span>
                 <p class="m0">
                 <small><strong><a href="#" class="text">'.$campid.'</a></strong>
                 </small>
+                <small class="text-muted pull-right ml" style="padding-right:20px;">'.$leadscount.'</small>
                 </p>';
     }
 }
