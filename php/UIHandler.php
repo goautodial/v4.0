46b5ed7329f20041dec1a90b9b6d7fe80bef9710
Alexander Abenoja <alexandero.oabenoja@gmail.com>
2016-08-18 09:17:00 +0800

    Added Agent Rank View in Ingroup

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -3397,6 +3397,31 @@ error_reporting(E_ERROR | E_PARSE);
 		return $output;
 		
 	}
+
+	public function API_goGetAllAgentRank($user_id, $group_id) {
+		$url = gourl."/goInbound/goAPI.php"; #URL to GoAutoDial API. (required)
+		$postfields["goUser"] = goUser; #Username goes here. (required)
+		$postfields["goPass"] = goPass; #Password goes here. (required)
+		$postfields["goAction"] = "goGetAllAgentRank"; #action performed by the [[API:Functions]]. (required)
+		$postfields["responsetype"] = responsetype; #json. (required)
+		$postfields["user_id"] = $user_id;
+		$postfields["group_id"] = $group_id;
+		//$postfields["goVarLimit"] = 10;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);		
+		curl_setopt($ch, CURLOPT_POST, 1);		
+		curl_setopt($ch, CURLOPT_TIMEOUT, 100);		
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);		
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);		
+		$data = curl_exec($ch);
+		curl_close($ch);		
+		$output = json_decode($data);		 
+		
+		return $output;
+		
+	}
 	
 	/*
 	 *
