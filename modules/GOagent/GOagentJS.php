2bd50939db2a8e3228168fac5002495b44ddd7e9
Chris L <chris@iknowitworks.org>
2016-09-20 07:44:09 +0800

    Updates of GOagent Dialer

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -19,8 +19,8 @@ $NOW_TIME = date("Y-m-d H:i:s");
 $tsNOW_TIME = date("YmdHis");
 $StarTtimE = date("U");
 
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
+//ini_set('display_errors', 'on');
+//error_reporting(E_ALL);
 
 $result = get_user_info($_SESSION['user']);
 $default_settings = $result->default_settings;
