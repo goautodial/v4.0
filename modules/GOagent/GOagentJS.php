2bd3883f857ba3c81346812e5e22f34ee3597329
Chris L <chris@iknowitworks.org>
2016-09-28 00:24:27 +0800

    Updates for custom fields

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -6902,6 +6902,7 @@ String.prototype.toUpperFirst = function() {
             case "CustoMFielD":
                 $list_id = $_REQUEST['list_id'];
                 $result = $ui->API_goGetAllCustomFields($list_id);
+                var_dump($result);
                 break;
         }
         print($result);
