b1a72fdcd9897b9fb487ecccd308d1c51d3d70d5
demian <demian@goautodial.com>
2016-10-18 07:25:10 -0400

    Update for Dashboard map

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ $goAPI = (empty($_SERVER['HTTPS'])) ? str_replace('https:', 'http:', gourl) : st
 				<!-- =============== APP STYLES ===============-->
 			<link rel="stylesheet" href="theme_dashboard/css/app.css" id="maincss">
 		
-                        <link rel="stylesheet" href="adminlte/css/AdminLTE.min.css">
+            <link rel="stylesheet" href="adminlte/css/AdminLTE.min.css">
 		<!-- preloader -->
         <link rel="stylesheet" href="css/customizedLoader.css">
 
@@ -922,7 +922,75 @@ $callsperhour = $ui->API_goGetCallsPerHour();
 			}
 		  });
 		  /* END JQUERY KNOB */
-		  $('#world-map').vectorMap({map: 'world_mill_en'});
+			var series = {
+				'CA': 11100,   // Canada
+				'DE': 2510,    // Germany
+				'FR': 3710,    // France
+				'AU': 5710,    // Australia
+				'GB': 8310,    // Great Britain
+				'RU': 9310,    // Russia
+				'BR': 6610,    // Brazil
+				'IN': 7810,    // India
+				'CN': 4310,    // China
+				'US': 839,     // USA
+				'SA': 410      // Saudi Arabia
+			};
+		  $('#world-map').vectorMap({
+			map: 'world_mill_en',
+			backgroundColor: 'transparent',
+            regionStyle: {
+				initial: {
+					'fill':           '#bbbec6',
+					'fill-opacity':   1,
+					'stroke':         'none',
+					'stroke-width':   1.5,
+					'stroke-opacity': 1
+				},
+				hover: {
+					'fill-opacity': 0.8
+				},
+				selected: {
+					fill: 'blue'
+				},
+				selectedHover: {
+				}
+            },
+			focusOn:{ x:0.4, y:0.6, scale: 1},
+            markerStyle: {
+				initial: {
+					fill: '#23b7e5',
+					stroke: '#23b7e5'
+				}
+            },
+            onRegionLabelShow: function(e, el, code) {
+				if ( series && series[code] )
+					el.html(el.html() + ': ' + series[code] + ' visitors');
+            },
+            markers: [
+				{ latLng:[41.90, 12.45],  name:'Vatican City'          },
+				{ latLng:[43.73, 7.41],   name:'Monaco'                },
+				{ latLng:[-0.52, 166.93], name:'Nauru'                 },
+				{ latLng:[-8.51, 179.21], name:'Tuvalu'                },
+				{ latLng:[7.11,171.06],   name:'Marshall Islands'      },
+				{ latLng:[17.3,-62.73],   name:'Saint Kitts and Nevis' },
+				{ latLng:[3.2,73.22],     name:'Maldives'              },
+				{ latLng:[35.88,14.5],    name:'Malta'                 },
+				{ latLng:[41.0,-71.06],   name:'New England'           },
+				{ latLng:[12.05,-61.75],  name:'Grenada'               },
+				{ latLng:[13.16,-59.55],  name:'Barbados'              },
+				{ latLng:[17.11,-61.85],  name:'Antigua and Barbuda'   },
+				{ latLng:[-4.61,55.45],   name:'Seychelles'            },
+				{ latLng:[7.35,134.46],   name:'Palau'                 },
+				{ latLng:[42.5,1.51],     name:'Andorra'               }
+			],
+            series: {
+                regions: [{
+                    values: series,
+                    scale: ['#878c9a'],
+                    normalizeFunction: 'polynomial'
+                }]
+            }
+		  });
 		});
 </script>
 	
