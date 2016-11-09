7f1241b9197e073a0f0e21f40984932049c6a8d2
demian <demian@goautodial.com>
2016-11-09 05:21:43 -0500

    Updates for Customer Convertion of leads

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -7668,8 +7668,9 @@ function ViewCustInfo(leadid) {
                 }
                 $("#customer-info-content").html(infoHtml).slideDown();
                 $("#cust-info-submit").prop('disabled', false);
-                if (lead_info.list_id > 100 && lead_info.list_id < 995) {
+                if (result.is_customer > 0) {
                     $("#convert-customer").prop('checked', true);
+                    $("#convert-customer").prop('disabled', true);
                 }
             }, 2000);
         } else {
