1f58184d42e12d861dfea551a2fee5ddc853fc0f
demian <demian@goautodial.com>
2016-11-18 12:06:27 -0500

    updated version for uploading leads

--- php/AddLoadLeads.php
+++ php/AddLoadLeads.php
@@ -9,53 +9,51 @@
 	 * @param responsetype
 	 * @param files
 	 */
-
+ini_set('memory_limit','1024M');
+	ini_set('upload_max_filesize', '6000M');
+	ini_set('post_max_size', '6000M');
+	
 	require_once('goCRMAPISettings.php');
 	require_once('CRMDefaults.php');
 	
 	// print_r($_FILES['file_upload']);die();
-    $url = gourl."/goUploadLeads/goAPI.php"; #URL to GoAutoDial API. (required)
-    
-    $postfields["goUser"]         = goUser; #Username goes here. (required)
-    $postfields["goPass"]         = goPass; #Password goes here. (required)
-    $postfields["goAction"]       = "goUploadMe"; #action performed by the [[API:Functions]]. (required)
-    $postfields["responsetype"]   = responsetype; #json. (required)
-    $postfields["hostname"]       = $_SERVER['REMOTE_ADDR']; #Default value
-    $postfields["goFileMe"]       = curl_file_create($_FILES['file_upload']['tmp_name'], $_FILES['file_upload']['type'], $_FILES["file_upload"]["name"]);
-    $postfields["goListId"]       = $_REQUEST['list_id'];
-    $postfields["goDupcheck"]       = $_REQUEST['goDupcheck'];
+	$url = gourl."/goUploadLeads/goAPI.php"; #URL to GoAutoDial API. (required)
 	
-    // print_r($postfields);die;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    //curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 100);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-    $data = curl_exec($ch);
-    // print_r(curl_getinfo($ch));die;
-    curl_close($ch);
-    
-    $output = json_decode($data);
-
-    $home = "../telephonylist.php";
-   
-    if ($output->result == "success") {
-    	// echo "Success";
-		#var_dump($output);
-		#die('hoy1');
-    	header("Location: ".$home."?message=success&RetMesg=".$output->message);
-		#header("Location: ".$home."?message=Success&query=".$output->query);
-    } else {
-    	// echo "The following error occured: ".$output->result;
-		#var_dump($output);
-		#die('hoy2');
-		header("Location: ".$home."?message=error&RetMesg=".$output->message);
-    	
-    }
-
-	#print_r($data);
-	#ie;
+	$postfields["goUser"]         = goUser; #Username goes here. (required)
+	$postfields["goPass"]         = goPass; #Password goes here. (required)
+	$postfields["goAction"]       = "goUploadMe"; #action performed by the [[API:Functions]]. (required)
+	$postfields["responsetype"]   = responsetype; #json. (required)
+	$postfields["hostname"]       = $_SERVER['REMOTE_ADDR']; #Default value
+	$postfields["goFileMe"]       = curl_file_create($_FILES['file_upload']['tmp_name'], $_FILES['file_upload']['type'], $_FILES["file_upload"]["name"]);
+	$postfields["goListId"]       = $_REQUEST['list_id'];
+	$postfields["goDupcheck"]       = $_REQUEST['goDupcheck'];
+	$home = "../telephonylist.php";
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT , 0); //gg
+	curl_setopt ($ch, CURLOPT_TIMEOUT  , 10000); //gg
+	$data = curl_exec($ch);
+	
+	curl_close($ch);
+	$output = json_decode($data);
+	
+	if ($output->result == "success") {
+		
+		//header("Location: ".$home."?message=success&RetMesg=".$output->message);
+		$status = $output->message;
+	} else {
+		
+		#header("Location: ".$home."?message=error&RetMesg=".$output->message);
+		$status = $output->message;
+		
+	}
+	
+	echo $status;
 
 ?>
