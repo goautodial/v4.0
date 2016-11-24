89161f3bcf2b68f02f7eed9d23e52164846cb9cb
root <root@demo-fr.goautodial.com>
2016-11-24 14:45:01 +0100

    Updates for usergroups

--- settingsusergroups.php
+++ settingsusergroups.php
@@ -284,7 +284,7 @@
 
             // disable special characters on Usergroup ID   
                 $('#usergroup_id').bind('keypress', function (event) {
-                    var regex = new RegExp("^[ A-Za-z0-9]+$");
+                    var regex = new RegExp("^[A-Za-z0-9]+$");
                     var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
                     if (!regex.test(key)) {
                        event.preventDefault();
