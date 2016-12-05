5f4140b0d2cbc1f7e063bfe21c6c4cfa74c3ab4b
Noel Umandap <noelumandap@gmail.com>
2016-12-06 03:57:22 +0800

    adjustment edit telephony list

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -5808,6 +5808,50 @@ error_reporting(E_ERROR | E_PARSE);
 		return $status;
 	}
 
+	public function API_ListsStatuses($list_id) {
+		$url = gourl."/goLists/goAPI.php"; #URL to GoAutoDial API. (required)
+		$postfields["goUser"] = goUser; #Username goes here. (required)
+		$postfields["goPass"] = goPass;
+		$postfields["goAction"] = "goGetStatusesWithCountCalledNCalled"; #action performed by the [[API:Functions]]
+		$postfields["responsetype"] = responsetype;
+		$postfields["list_id"] = $list_id;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+		$data = curl_exec($ch);
+		curl_close($ch);
+
+		$output = json_decode($data);
+
+		return $output;
+	}
+
+	public function API_ListsTimezone($list_id) {
+		$url = gourl."/goLists/goAPI.php"; #URL to GoAutoDial API. (required)
+		$postfields["goUser"] = goUser; #Username goes here. (required)
+		$postfields["goPass"] = goPass;
+		$postfields["goAction"] = "goGetTZonesWithCountCalledNCalled"; #action performed by the [[API:Functions]]
+		$postfields["responsetype"] = responsetype;
+		$postfields["list_id"] = $list_id;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+		$data = curl_exec($ch);
+		curl_close($ch);
+
+		$output = json_decode($data);
+
+		return $output;
+	}
+
 }
 
 ?>
