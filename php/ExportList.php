84514679a573bca00a151b249f89a64b01d5dcef
demian <demian@goautodial.com>
2016-12-07 14:19:38 -0500

    Memory Limit

--- php/ExportList.php
+++ php/ExportList.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
     }*/
-    
+    ini_set('memory_limit', '2048M');
     require_once('./goCRMAPISettings.php');
 	
     $url = gourl."/goLists/goAPI.php"; #URL to GoAutoDial API. (required)
