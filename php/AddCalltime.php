7ebf02abb1bad75f232b1938644872cbd714fba8
demian <demian@goautodial.com>
2016-12-08 21:12:43 -0500

    Fixed the bug on issue #3756

--- php/AddCalltime.php
+++ php/AddCalltime.php
@@ -13,29 +13,29 @@
 
         $url = gourl."/goCalltimes/goAPI.php"; #URL to GoAutoDial API. (required)
         
-        $start_default =  date('Hi', strtotime($_POST['start_default']));
-        $stop_default =  date('Hi', strtotime($_POST['stop_default']));
+        $start_default =	(strlen($_POST['start_default']) > 0) ? date('Hi', strtotime($_POST['start_default'])) : "0000";
+        $stop_default =		(strlen($_POST['stop_default']) > 0) ? date('Hi', strtotime($_POST['stop_default'])) : "0000";
 
-        $start_sunday =  date('Hi', strtotime($_POST['start_sunday']));
-        $stop_sunday =  date('Hi', strtotime($_POST['stop_sunday']));
+        $start_sunday =		(strlen($_POST['start_sunday']) > 0) ? date('Hi', strtotime($_POST['start_sunday'])) : $start_default;
+        $stop_sunday =		(strlen($_POST['stop_sunday']) > 0) ? date('Hi', strtotime($_POST['stop_sunday'])) : $stop_default;
 
-        $start_monday =  date('Hi', strtotime($_POST['start_monday']));
-        $stop_monday =  date('Hi', strtotime($_POST['stop_monday']));
+        $start_monday =		(strlen($_POST['start_monday']) > 0) ? date('Hi', strtotime($_POST['start_monday'])) : $start_default;
+        $stop_monday =		(strlen($_POST['stop_monday']) > 0) ? date('Hi', strtotime($_POST['stop_monday'])) : $stop_default;
 
-        $start_tuesday =  date('Hi', strtotime($_POST['start_tuesday']));
-        $stop_tuesday =  date('Hi', strtotime($_POST['stop_tuesday']));
+        $start_tuesday =	(strlen($_POST['start_tuesday']) > 0) ? date('Hi', strtotime($_POST['start_tuesday'])) : $start_default;
+        $stop_tuesday =		(strlen($_POST['stop_tuesday']) > 0) ? date('Hi', strtotime($_POST['stop_tuesday'])) : $stop_default;
 
-        $start_wednesday =  date('Hi', strtotime($_POST['start_wednesday']));
-        $stop_wednesday =  date('Hi', strtotime($_POST['stop_wednesday']));
+        $start_wednesday =	(strlen($_POST['start_wednesday']) > 0) ? date('Hi', strtotime($_POST['start_wednesday'])) : $start_default;
+        $stop_wednesday =	(strlen($_POST['stop_wednesday']) > 0) ? date('Hi', strtotime($_POST['stop_wednesday'])) : $stop_default;
 
-        $start_thursday =  date('Hi', strtotime($_POST['start_thursday']));
-        $stop_thursday =  date('Hi', strtotime($_POST['stop_thursday']));
+        $start_thursday =	(strlen($_POST['start_thursday']) > 0) ? date('Hi', strtotime($_POST['start_thursday'])) : $start_default;
+        $stop_thursday =	(strlen($_POST['stop_thursday']) > 0) ? date('Hi', strtotime($_POST['stop_thursday'])) : $stop_default;
 
-        $start_friday =  date('Hi', strtotime($_POST['start_friday']));
-        $stop_friday =  date('Hi', strtotime($_POST['stop_friday']));
+        $start_friday =		(strlen($_POST['start_friday']) > 0) ? date('Hi', strtotime($_POST['start_friday'])) : $start_default;
+        $stop_friday =		(strlen($_POST['stop_friday']) > 0) ? date('Hi', strtotime($_POST['stop_friday'])) : $stop_default;
 
-        $start_saturday =  date('Hi', strtotime($_POST['start_saturday']));
-        $stop_saturday =  date('Hi', strtotime($_POST['stop_saturday']));
+        $start_saturday =	(strlen($_POST['start_saturday']) > 0) ? date('Hi', strtotime($_POST['start_saturday'])) : $start_default;
+        $stop_saturday =	(strlen($_POST['stop_saturday']) > 0) ? date('Hi', strtotime($_POST['stop_saturday'])) : $stop_default;
 
         $postfields["goUser"]             = goUser; #Username goes here. (required)
         $postfields["goPass"]             = goPass; #Password goes here. (required)
