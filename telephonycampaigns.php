4158d291b1cbe655be93aacf3f7f52c452aa5a6c
Chris L <chris@goautodial.com>
2017-01-25 10:28:09 +0800

    Fix for the bug #4012

--- telephonycampaigns.php
+++ telephonycampaigns.php
@@ -450,7 +450,7 @@
 				    				<div class="col-lg-8 mb">
 				    					<div class="input-group">
 									      <input id="campaign-id" name="campaign_id" type="number" class="form-control" placeholder="" value="<?php echo str_pad(mt_rand(1,99999999),8,'0',STR_PAD_LEFT); ?>" min="0" minlength="3" maxlength="8" readonly onkeydown="return FilterInput(event)">
-									      <span class="input-group-btn">
+									      <span class="input-group-btn" style="vertical-align: top;">
 									        <button id="campaign-id-edit-btn" class="btn btn-default" type="button" style="min-height: 34px;"><i class="fa fa-pencil"></i></button>
 									      </span>
 									    </div><!-- /input-group -->
