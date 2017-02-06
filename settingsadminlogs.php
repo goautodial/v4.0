509090f5b29e5e51c1cbfbd59fb90ebad4213031
Chris L <chris@goautodial.com>
2017-02-06 13:09:08 +0800

    Updates for Admin Logs

--- settingsadminlogs.php
+++ settingsadminlogs.php
@@ -11,7 +11,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title><?php $lh->translateText('portal_title'); ?></title>
+        <title><?php $lh->translateText('portal_title'); ?> - Admin Logs</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
         <?php print $ui->standardizedThemeCSS(); ?>
