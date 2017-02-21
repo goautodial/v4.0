596cc48b14fe760c12fba57963dfb1508a7c5cfe
Chris L <chris@goautodial.com>
2017-02-21 20:31:28 +0800

    Update for Admin-LTE

--- telephonyscripts.php
+++ telephonyscripts.php
@@ -92,8 +92,8 @@
 	/*
 	* APIs for add form
 	*/
-	$scripts = $ui->API_goGetAllScripts($user->getUserName());
-	var_dump($user->getUserName());
+	$scripts = $ui->API_goGetAllScripts($_SESSION['user']);
+	var_dump($_SESSION['user']);
 
 ?>
 	<div class="modal fade" id="scripts-modal" tabindex="-1"aria-labelledby="scripts">
