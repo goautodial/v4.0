2015709dc0f04d59b8f9fa1a9c12f71fb8307026
Chris L <chris@goautodial.com>
2017-03-07 12:31:58 +0800

    For Export list bug

--- php/ExportList.php
+++ php/ExportList.php
@@ -26,7 +26,6 @@ error_reporting(E_ALL);
 //    echo "<pre>";
 //	print_r($output);
 //	echo "</pre>";
-var_dump($output);
 	
     if($output->result == "success"){
         //$filename = $output->getReports->filename;
