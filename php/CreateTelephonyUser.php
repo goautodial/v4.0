3fdbb18ea546684648b6b0b03c9d4609fa101d3d
demian <demian@goautodial.com>
2017-03-15 18:43:45 -0400

    Multiple Add user

--- php/CreateTelephonyUser.php
+++ php/CreateTelephonyUser.php
@@ -1,28 +1,26 @@
 <?php
 
-	/** Telephony Users API - Add a new Telephony User */
-	/**
-	 * Generates action circle buttons for different pages/module
-	 */
+/** Telephony Users API - Add a new Telephony User */
+
 require_once('goCRMAPISettings.php');	
 
 	$url = gourl."/goUsers/goAPI.php"; # URL to GoAutoDial API file
-	$postfields["goUser"] 			= goUser; #Username goes here. (required)
-	$postfields["goPass"] 			= goPass; #Password goes here. (required)
-	$postfields["goAction"] 		= "goAddUser"; #action performed by the [[API:Functions]]
+	$postfields["goUser"] 	= goUser; #Username goes here. (required)
+	$postfields["goPass"] 	= goPass; #Password goes here. (required)
+	$postfields["goAction"] 	= "goAddUser"; #action performed by the [[API:Functions]]
 	$postfields["responsetype"] 	= responsetype; #json (required)
-	$postfields["hostname"] 		= $_SERVER['REMOTE_ADDR']; #Default value
+	$postfields["hostname"] 	= $_SERVER['REMOTE_ADDR']; #Default value
 	
-	$postfields["user"] 			= $_POST['user_form']; 
-	$postfields["pass"] 			= $_POST['password']; 
+	$postfields["user"] 		= $_POST['user_form']; 
+	$postfields["pass"] 		= $_POST['password']; 
 	$postfields["full_name"] 		= $_POST['fullname']; 
 	$postfields["user_group"] 		= $_POST['user_group']; 
-	$postfields["active"] 			= $_POST['status']; 
-	$postfields["seats"]			= $_POST["seats"];
+	$postfields["active"] 		= $_POST['status']; 
+	$postfields["seats"]		= $_POST["seats"];
 	$postfields["phone_login"]		= $_POST["phone_logins"];
 	$postfields["phone_pass"]		= $_POST["phone_pass"];
 	
-	$postfields["log_user"]			= $_POST["log_user"];
+	$postfields["log_user"]		= $_POST["log_user"];
 	$postfields["log_group"]		= $_POST["log_group"];
 
 	$ch = curl_init();
@@ -47,7 +45,6 @@ require_once('goCRMAPISettings.php');
 		// $return['msg'] = "Something went wrong please see input data on form.";
         $status = $output->result;
 	}
-
 	echo $status;
 
 ?>
