0fed02de9c74d4676a0e8720212df68161c5356f
Chris L <chris@goautodial.com>
2017-03-17 12:57:51 +0800

    Updates for Admin-LTE GOagent

--- modules/GOagent/module.php
+++ modules/GOagent/module.php
@@ -536,7 +536,7 @@ EOF;
 	}, function successCb(stream) {
 		localStream = stream;
 	
-		phone.start();
+		//phone.start();
 	}, function failureCb(e) {
 		console.error('getUserMedia failed.', e);
 	});
