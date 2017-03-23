a495bf4b73b29af027cd02e6fbb1c7fe6aa5e4da
demian <demian@goautodial.com>
2017-03-23 14:37:58 -0400

    update in html meta + adjustments in login page css

--- callrecordings.php
+++ callrecordings.php
@@ -22,7 +22,7 @@
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php $lh->translateText("call_recordings"); ?></title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
--- callreports.php
+++ callreports.php
@@ -19,7 +19,7 @@
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php $lh->translateText("reports_and_go_analytics"); ?></title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         
