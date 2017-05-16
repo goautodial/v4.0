4588117b072ae57c5123ca9b18eca9c235b2ac3c
Chris L <chris@goautodial.com>
2017-05-16 15:03:09 +0800

    Minor revisions

--- login.php
+++ login.php
@@ -41,7 +41,96 @@
 	require_once('./php/DbHandler.php');
 	require_once('./php/LanguageHandler.php');
 	
-	var_dump(CRM_SESSION_DRIVER);
+
+	if (CRM_SESSION_DRIVER == 'database') {
+		function on_session_start($save_path, $session_name) {
+			//error_log($session_name . " ". session_id());
+			error_log("Session created: $session_name");
+		}
+		
+		function on_session_end() {
+			// Noting to do here...
+			error_log("Session closed.");
+		}
+		
+		function on_session_read($id) {
+			global $con;
+			error_log($id);
+			//$stmt = "SELECT session_data from sessions ";
+			//$stmt .= "where session_id ='$key' ";
+			//$stmt .= "and unix_timestamp(session_expiration) > unix_timestamp(date_add(now(),interval 1 hour))";
+			//$sth = mysql_query($stmt);
+			
+			//$con->where('id', $id);
+			//$con->where('last_activity', 'UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 1 HOUR))', '>');
+			//$result = $con->getOne('go_sessions', 'user_data');
+			//
+			//if ($result) {
+			//	return($result['user_data']);
+			//} else {
+			//	return $result;
+			//}
+			return true;
+		}
+		
+		function on_session_write($id, $data) {
+			global $con;
+			error_log("$id = $data");
+			//$insert_stmt  = "insert into sessions values('$key', ";
+			//$insert_stmt .= "'$val',unix_timestamp(date_add(now(), interval 1 hour)))";
+			//
+			//$update_stmt  = "update sessions set session_data ='$val', ";
+			//$update_stmt .= "session_expiration = unix_timestamp(date_add(now(), interval 1 hour))";
+			//$update_stmt .= "where session_id ='$key '";
+			//
+			//// First we try to insert, if that doesn't succeed, it means
+			//// session is already in the table and we try to update
+			//
+			//
+			////mysql_query($insert_stmt);
+			//$con->insert
+			//
+			////$err = mysql_error();
+			//
+			//if ($err != 0)
+			//{
+			//	error_log( mysql_error());
+			//	mysql_query($update_stmt);
+			//}
+		}
+		
+		function on_session_destroy($id) {
+			global $con;
+			var_dump("Session destroyed.");
+			//$id = mysql_real_escape_string($id);
+			//$sql = "DELETE
+			//	   FROM   sessions
+			//	   WHERE  id = '$id'";
+			//return mysql_query($sql, $con);
+			return true;
+		}
+		 
+		function on_session_gc($max) {
+			global $con;
+			var_dump("Session cleaned.");
+		
+			//$old = time() - $max;
+			//$old = mysql_real_escape_string($old);
+			//
+			//$sql = "DELETE
+			//	   FROM   sessions
+			//	   WHERE  access < '$old'";
+			//
+			//return mysql_query($sql, $con);
+		}
+		
+		session_set_save_handler('on_session_start',
+							'on_session_end',
+							'on_session_read',
+							'on_session_write',
+							'on_session_destroy',
+							'on_session_gc');
+	}
 
 	session_start(); // Starting Session
 	$lh = \creamy\LanguageHandler::getInstance();
