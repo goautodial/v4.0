0c0498a4d5cae598efae498e81f08d1fcda00f87
Chris L <chris@goautodial.com>
2017-05-16 09:56:28 +0800

    Minor revision on sessions

--- php/Session.php
+++ php/Session.php
@@ -33,6 +33,7 @@ if (version_compare(phpversion(), '5.4.0', '<')) {
      }
 } else {
 	if (session_status() == PHP_SESSION_NONE) {
+		var_dump('test');
 		if (CRM_SESSION_DRIVER == 'database') {
 			function on_session_start($save_path, $session_name) {
 				//error_log($session_name . " ". session_id());
