7ce5c243b25b9a11e0841cf7ece239125b8105c0
Chris L <chris@goautodial.com>
2017-05-17 14:19:24 +0800

    Minor revisions on storing sessions

--- php/Session.php
+++ php/Session.php
@@ -25,6 +25,17 @@
 
 namespace creamy;
 
+if (CRM_SESSION_DRIVER == 'database') {
+	require_once('./php/SessionHandler.php');
+	$session_class = new \creamy\SessionHandler();
+	session_set_save_handler(array(&$session_class, 'open'),
+							 array(&$session_class, 'close'),
+							 array(&$session_class, 'read'),
+							 array(&$session_class, 'write'),
+							 array(&$session_class, 'destroy'),
+							 array(&$session_class, 'gc'));
+}
+
 // Start session if not already started.
 if (version_compare(phpversion(), '5.4.0', '<')) {
      if(session_id() == '') {
