162e774ed13a8bf27711f3b9202813f94bd89a25
Chris L <chris@goautodial.com>
2017-05-17 15:34:06 +0800

    Minor revisions on storing sessions

--- php/SessionHandler.php
+++ php/SessionHandler.php
@@ -2,9 +2,9 @@
 namespace creamy;
 
 // dependencies
-require_once('CRMDefaults.php');
-require_once('LanguageHandler.php');
-require_once('DbHandler.php');
+//require_once('CRMDefaults.php');
+//require_once('LanguageHandler.php');
+//require_once('DbHandler.php');
 
 class SessionHandler {
     // ****************************************************************************
