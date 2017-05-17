4f0e87271adab7c9c52ec6a2a0d3b9e189d5787b
Chris L <chris@goautodial.com>
2017-05-17 15:43:08 +0800

    Minor revisions on storing sessions

--- login.php
+++ login.php
@@ -43,18 +43,64 @@
 	
 
 	if (CRM_SESSION_DRIVER == 'database') {
-		require_once('./php/SessionHandler.php');
-		$session_class = new \creamy\SessionHandler();
-		//session_set_save_handler(array(&$session_class, 'openSession'),
-		//						 array(&$session_class, 'closeSession'),
-		//						 array(&$session_class, 'readSession'),
-		//						 array(&$session_class, 'writeSession'),
-		//						 array(&$session_class, 'destroySession'),
-		//						 array(&$session_class, 'gcSession'));
+		function on_session_start($save_path, $session_name) {
+			//error_log($session_name . " ". session_id());
+			//error_log("Session created: $session_name");
+		}
+		
+		function on_session_end() {
+			// Noting to do here...
+			//error_log("Session closed.");
+		}
+		
+		function on_session_read($id) {
+			$sDB = new \creamy\DbHandler();
+			//error_log($id);
+			$result = $sDB->onSessionRead($id);
+			
+			if ($result) {
+				return($result['user_data']);
+			} else {
+				return $result;
+			}
+		}
+		
+		function on_session_write($id, $data) {
+			$sDB = new \creamy\DbHandler();
+			$postData = array(
+				'session_id' => $sDB->escape_string($id),
+				'user_data' => $sDB->escape_string($data),
+				'last_activity' => time(),
+				'ip_address' => $_SERVER['REMOTE_ADDR'],
+				'user_agent' => $_SERVER['HTTP_USER_AGENT']
+			);
+			
+			$result = $sDB->onSessionWrite($postData);
+		}
+		
+		function on_session_destroy($id) {
+			$sDB = new \creamy\DbHandler();
+			//var_dump("Session destroyed.");
+			$result = $sDB->onSessionDestroy($id);
+			return $result;
+		}
+		 
+		function on_session_gc($max) {
+			global $con;
+			//var_dump("Session cleaned.");
+			$result = $sDB->onSessionGC($max);
+			return true;
+		}
+		
+		session_set_save_handler('on_session_start',
+							'on_session_end',
+							'on_session_read',
+							'on_session_write',
+							'on_session_destroy',
+							'on_session_gc');
 	}
 
 	session_start(); // Starting Session
-	
 	$lh = \creamy\LanguageHandler::getInstance();
 	$ui = \creamy\UIHandler::getInstance();
 	$error = ''; // Variable To Store Error Message
