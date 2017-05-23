6008c91aac14b3b47a4b3e6cac7725b572dc9ebd
Chris L <chris@goautodial.com>
2017-05-23 13:55:56 +0800

    Minor revisions

--- modules/GOagent/module.php
+++ modules/GOagent/module.php
@@ -370,7 +370,7 @@ EOF;
 			
 			preg_match("/Chrome\/(\d+)/", $_SERVER['HTTP_USER_AGENT'], $matches);
 			$rtcpMuxPolicy = '';
-			if ($matches[1] < 58) {
+			if ($matches[1] < 57) {
 				$rtcpMuxPolicy = 'rtcpMuxPolicy: "negotiate"';
 			}
 			$str .= <<<EOF
