7dfd20add8198216a90371709b8c2e59521e14cc
Chris L <chris@goautodial.com>
2017-06-09 10:46:18 +0800

    Added the jsSIP libraries

--- modules/GOagent/module.php
+++ modules/GOagent/module.php
@@ -969,7 +969,7 @@ EOF;
 			}
 			$str .= <<<EOF
 <audio id="remoteStream" style="display: none;" autoplay controls></audio>
-<script type="text/javascript" src="{$goModuleDIR}js/plugins/jsSIP/lib/JsSIP.js"></script>
+<script type="text/javascript" src="{$goModuleDIR}js/jsSIP/lib/JsSIP.js"></script>
 <script type="text/javascript" src="{$goModuleDIR}js/rtcninja.js"></script>
 <script>
 	var audioElement = document.querySelector('#remoteStream');
