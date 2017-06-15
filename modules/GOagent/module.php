5d17d1d207db504ef45e4706a12dbb5e2c0f64db
Chris L <chris@goautodial.com>
2017-06-15 22:54:26 +0800

    Minor revision

--- modules/GOagent/module.php
+++ modules/GOagent/module.php
@@ -463,15 +463,15 @@ EOF;
 			});
 		});
 	
-		session.on('addstream', function (data) {
-			console.log('session::addstream', data);
-		
-			remoteStream = data.stream;
-			audioElement = document.querySelector('#remoteStream');
-			audioElement.src = window.URL.createObjectURL(remoteStream);
-			
-			globalSession = session;
-		});
+		//session.on('addstream', function (data) {
+		//	console.log('session::addstream', data);
+		//
+		//	remoteStream = data.stream;
+		//	audioElement = document.querySelector('#remoteStream');
+		//	audioElement.src = window.URL.createObjectURL(remoteStream);
+		//	
+		//	globalSession = session;
+		//});
 	
 		session.on('removestream', function (data) {
 			console.log('session::removestream', data);
@@ -537,15 +537,15 @@ EOF;
 		//		rtcpMuxPolicy: "negotiate"
 		//	}
 		
-		//session.connection.addEventListener('addstream', (event) => {
-		//	console.log("session::addstream", event);
-		//	
-		//	remoteStream = event.stream;
-		//	audioElement = document.querySelector('#remoteStream');
-		//	audioElement.srcObject = remoteStream;
-		//	
-		//	globalSession = session;
-		//});
+		session.connection.addEventListener('addstream', (event) => {
+			console.log("session::addstream", event);
+			
+			remoteStream = event.stream;
+			audioElement = document.querySelector('#remoteStream');
+			audioElement.srcObject = remoteStream;
+			
+			globalSession = session;
+		});
 	});
 	
 	phone.on('newMessage', function(e) {
