27182c5738ce3b3662253335268d11ff2c5f4cf4
Chris L <chris@goautodial.com>
2017-06-16 03:11:20 +0800

    Minor revisions

--- modules/GOagent/module.php
+++ modules/GOagent/module.php
@@ -525,7 +525,6 @@ EOF;
 				audio: true,
 				video: false
 			},
-			mediaStream: localStream,
 			pcConfig: {
 				rtcpMuxPolicy: "negotiate"
 			},
@@ -582,17 +581,17 @@ EOF;
 		}
 	});
 	
-	navigator.getUserMedia({
-		audio: true,
-		video: false
-	}, function (stream) {
-		localStream = stream;
-		console.log('getUserMedia', stream);
-	
-		//phone.start();
-	}, function (err) {
-		console.error('getUserMedia failed: %s', err.toString());
-	});
+	//navigator.getUserMedia({
+	//	audio: true,
+	//	video: false
+	//}, function (stream) {
+	//	localStream = stream;
+	//	console.log('getUserMedia', stream);
+	//
+	//	//phone.start();
+	//}, function (err) {
+	//	console.error('getUserMedia failed: %s', err.toString());
+	//});
 </script>
 EOF;
 		}
