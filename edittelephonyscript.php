9e351c9257cfdbf87347769d9fdd9c37b97fc057
demian <demian@goautodial.com>
2017-07-12 16:04:12 -0400

    Fixed Insert Script Text

--- edittelephonyscript.php
+++ edittelephonyscript.php
@@ -358,40 +358,41 @@
 			function addtext() {
 				var txtarea = document.getElementById('script_text');
 				var text = document.getElementById('script_text_dropdown').value;
+				CKEDITOR.instances.script_text.insertText( text );
 
-				if (!txtarea) { return; }
+				// if (!txtarea) { return; }
 
-				var scrollPos = txtarea.scrollTop;
-				var strPos = 0;
-				var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
-					"ff" : (document.selection ? "ie" : false ) );
-				if (br == "ie") {
-					txtarea.focus();
-					var range = document.selection.createRange();
-					range.moveStart ('character', -txtarea.value.length);
-					strPos = range.text.length;
-				} else if (br == "ff") {
-					strPos = txtarea.selectionStart;
-				}
+				// var scrollPos = txtarea.scrollTop;
+				// var strPos = 0;
+				// var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
+				// 	"ff" : (document.selection ? "ie" : false ) );
+				// if (br == "ie") {
+				// 	txtarea.focus();
+				// 	var range = document.selection.createRange();
+				// 	range.moveStart ('character', -txtarea.value.length);
+				// 	strPos = range.text.length;
+				// } else if (br == "ff") {
+				// 	strPos = txtarea.selectionStart;
+				// }
 
-				var front = (txtarea.value).substring(0, strPos);
-				var back = (txtarea.value).substring(strPos, txtarea.value.length);
-				txtarea.value = front + text + back;
-				strPos = strPos + text.length;
-				if (br == "ie") {
-					txtarea.focus();
-					var ieRange = document.selection.createRange();
-					ieRange.moveStart ('character', -txtarea.value.length);
-					ieRange.moveStart ('character', strPos);
-					ieRange.moveEnd ('character', 0);
-					ieRange.select();
-				} else if (br == "ff") {
-					txtarea.selectionStart = strPos;
-					txtarea.selectionEnd = strPos;
-					txtarea.focus();
-				}
+				// var front = (txtarea.value).substring(0, strPos);
+				// var back = (txtarea.value).substring(strPos, txtarea.value.length);
+				// txtarea.value = front + text + back;
+				// strPos = strPos + text.length;
+				// if (br == "ie") {
+				// 	txtarea.focus();
+				// 	var ieRange = document.selection.createRange();
+				// 	ieRange.moveStart ('character', -txtarea.value.length);
+				// 	ieRange.moveStart ('character', strPos);
+				// 	ieRange.moveEnd ('character', 0);
+				// 	ieRange.select();
+				// } else if (br == "ff") {
+				// 	txtarea.selectionStart = strPos;
+				// 	txtarea.selectionEnd = strPos;
+				// 	txtarea.focus();
+				// }
 				
-				txtarea.scrollTop = scrollPos;
+				// txtarea.scrollTop = scrollPos;
 			}
 		</script>
 
