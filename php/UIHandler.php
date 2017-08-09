bb53a6bada49fc1f30740e76ff80be4a19608fc6
demian <demian@goautodial.com>
2017-08-09 14:12:09 -0400

    adjustment telephony script

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -6534,6 +6534,32 @@ error_reporting(E_ERROR | E_PARSE);
 		
 		return json_encode($result);
 	}
+
+	// Getting all Standard Fields
+	public function API_getAllStandardFields(){
+        $url = gourl."/goScripts/goAPI.php"; #URL to GoAutoDial API. (required)
+		$postfields["goUser"] = goUser; #Username goes here. (required)
+		$postfields["goPass"] = goPass;
+		$postfields["goAction"] = "goGetStandardFields"; #action performed by the [[API:Functions]]
+		$postfields["responsetype"] = responsetype;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		$output = json_decode($data);
+
+		if($output->result == "success"){
+			return $output->field_name;
+		}else{
+			return "EMPTY";
+		}
+	}
 }
 
 ?>
