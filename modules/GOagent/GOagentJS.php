319190a540305816a90771fb6adedb584d300952
Chris Lomuntad <chris@iknowitworks.org>
2018-01-19 02:16:12 +0800

    Customization for issue #6018

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -7761,7 +7761,7 @@ function checkForCallbacks() {
             var swalContent = '';
             if (!value.seen && (minsBetween <= 5 && minsBetween >= 0)) {
                 swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Name:</strong> '+value.cust_name+'</div>';
-                swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Phone:</strong> '+phone_number_format(value.phone_number)+' <span style="float:right;"><a class="btn btn-sm btn-success" onclick="NewCallbackCall('+key+', '+value.lead_id+');"><i class="fa fa-phone"></i></a> &nbsp; <a class="btn btn-sm btn-primary" onclick="ShowCBDatePicker('+key+', \"'+value.callback_time+'\", \"'+value.comments+'\");"><i class="fa fa-calendar"></i></a></span></div>';
+                swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Phone:</strong> '+phone_number_format(value.phone_number)+' <span style="float:right;"><a class="btn btn-sm btn-success" onclick="NewCallbackCall('+key+', '+value.lead_id+');"><i class="fa fa-phone"></i></a> &nbsp; <a class="btn btn-sm btn-primary" onclick="ShowCBDatePicker('+key+', \"'+value.callback_time.trim()+'\", \"'+value.comments+'\");"><i class="fa fa-calendar"></i></a></span></div>';
                 swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Callback Date:</strong> '+value.callback_time+'</div>';
                 swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Last Call Date:</strong> '+value.entry_time+'</div>';
                 swalContent += '<div style="padding: 0 30px; text-align: left; line-height: 24px;"><strong>Comments:</strong> '+value.comments+'</div>';
