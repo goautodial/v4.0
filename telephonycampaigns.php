dbf53aacb11784340d9fb46183255d4c307ab4c0
Demian Biscocho <demian@goautodial.com>
2018-05-28 13:27:52 -0400

    Header License Campaign

--- telephonycampaigns.php
+++ telephonycampaigns.php
@@ -1,20 +1,30 @@
 <?php
-
-	###########################################################
-	### Name: telephonycampaigns.php 		   ###
-	### Functions: Manage Campaigns, Disposition		   ###
-	### Copyright: GOAutoDial Ltd. (c) 2011-2016		   ###
-	### Version: 4.0 		   ###
-	### Written by: Alexander Abenoja & Noel Umandap		   ###
-	### License: AGPLv2		   ###
-	###########################################################
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
+ /**
+ * @file 	telephonycampaigns.php
+ * @brief 	Manage Campaigns, Dispositions & etc.
+ * @copyright 	Copyright (C) GOautodial Inc.
+ * @author	Alexander Jim H. Abenoja  <alex@goautodial.com>
+ * @author     	Noel Umandap  <noelumandap@goautodial.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 	require_once('./php/UIHandler.php');
 	require_once('./php/CRMDefaults.php');
-    require_once('./php/LanguageHandler.php');
-    include('./php/Session.php');
+	require_once('./php/LanguageHandler.php');
+	include('./php/Session.php');
 
 	$ui = \creamy\UIHandler::getInstance();
 	$lh = \creamy\LanguageHandler::getInstance();
