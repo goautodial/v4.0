8489daf45cc50782c450c76a178d4c7fb0e0681e
Demian Biscocho <demian@goautodial.com>
2018-06-18 22:29:54 +0800

    AGPLv3 licensing header..

--- php/ModifyTelephonyUser.php
+++ php/ModifyTelephonyUser.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * @file        ModifyTelephonyUser.php
+ * @brief       Modify user account
+ * @copyright   Copyright (C) GOautodial Inc.
+ * @author      Alexander Jim H. Abenoja  <alex@goautodial.com>
+ * @author		Jerico James F. Milo <james@goautodial.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
