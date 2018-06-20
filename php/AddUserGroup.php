74cbd47641208c2ce56b82a2caa27b9f500266cc
demianb <demian@goautodial.com>
2018-06-21 05:51:13 +0800

    Fixed Add new Usergroup issue..

--- php/AddUserGroup.php
+++ php/AddUserGroup.php
@@ -6,31 +6,32 @@
 require_once('goCRMAPISettings.php');	
 
 	$url = gourl."/goUserGroups/goAPI.php"; # URL to GoAutoDial API file
-	$postfields["goUser"] 			= goUser; #Username goes here. (required)
-	$postfields["goPass"] 			= goPass; #Password goes here. (required)
-	$postfields["goAction"] 		= "goAddUserGroup"; #action performed by the [[API:Functions]]
-	$postfields["responsetype"] 	= responsetype; #json (required)
-	$postfields["hostname"] 		= $_SERVER['REMOTE_ADDR']; #Default value
 	
-	$postfields["user_group"] 		= $_POST['usergroup_id']; 
-	$postfields["group_name"] 		= $_POST['groupname']; 
-	$postfields["group_level"] 		= $_POST['grouplevel'];
-	
-	$postfields["log_user"] 		= $_POST['log_user']; 
-	$postfields["log_group"] 		= $_POST['log_group'];
+	$postfields = array(
+		'goUser' => goUser,
+		'goPass' => goPass,
+		'goAction' => 'goAddUserGroup',		
+		'responsetype' => responsetype,
+		'user_group' => $_POST['usergroup_id'],
+		'group_name' => $_POST['groupname'],
+		'group_level' => $_POST['grouplevel'],
+		'session_user' => $_POST['log_user'],
+		'log_group' => $_POST['log_group'],
+		'hostname' => $_SERVER['REMOTE_ADDR']
+	);		
 
+	// Call the API
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
 	$data = curl_exec($ch);
 	curl_close($ch);
-
-	$output = json_decode($data);
+    $output = json_decode($data);
 	
 	if ($output->result=="success") {
 		# Result was OK!
