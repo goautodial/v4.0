91493fdbe256c6e3986b3ce227d738c7643477c2
unknown <alexandero.oabenoja@gmail.com>
2018-06-30 15:48:57 +0800

    Updates

--- telephonyusers.php
+++ telephonyusers.php
@@ -1084,7 +1084,7 @@
 	                    function(isConfirm){   
 	                        if (isConfirm) { 
 							$.ajax({
-							  url: "./php/DeleteSettingsPhones.php",
+							  url: "./php/DeletePhones.php",
 							  type: 'POST',
 							  data: { 
 								exten_id: id,
@@ -1126,7 +1126,7 @@
 					function(isConfirm){
 						if (isConfirm) {
 							$.ajax({
-								url: "./php/DeleteSettingsPhones.php",
+								url: "./php/DeletePhones.php",
 								type: 'POST',
 								data: { 
 								exten_id: arr,
