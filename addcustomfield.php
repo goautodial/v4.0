12ca7ac85483679e3afbd7382b7e0402bc1a60a3
Chris Lomuntad <chris@iknowitworks.org>
2019-01-23 03:09:19 +0800

    Fix for the issue #6934

--- addcustomfield.php
+++ addcustomfield.php
@@ -218,8 +218,11 @@ if ($perm->customfields_read === 'N' && $perm->customfields_update === 'N' && $p
 
 															if ( ($A_field_type=='SELECT') or ($A_field_type=='MULTI') or ($A_field_type=='RADIO') or ($A_field_type=='CHECKBOX') )
 															{
+																var_dump($A_field_options);
 																$A_field_options = str_replace("\r\n", "\n", $A_field_options);
+																var_dump($A_field_options);
 																$field_options_array = explode("\n", $A_field_options);
+																var_dump($field_options_array);
 
 																$field_options_count = count($field_options_array);
 																$te=0;
