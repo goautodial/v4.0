e3e9d3bc41a2b6a2b157828558b1df1d7602ca4f
Chris <chris@iknowitworks.org>
2019-03-26 23:43:30 +0800

    Debugging Agent Call Logging...

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -5433,6 +5433,7 @@ error_reporting(E_ERROR | E_PARSE);
 			$postfields["goPass"] = goPass;
 			$postfields["goAction"] = "goGetCallsPerHour"; #action performed by the [[API:Functions]]
 			$postfields["session_user"] = $session_user; #action performed by the [[API:Functions]]
+   $postfields["responsetype"] = 'json';
 			
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
