2a8c9b65a79f0a41f59b74506a84a377d5e28f82
Chris <chris@iknowitworks.org>
2019-04-11 04:41:37 +0800

    Fixed the issue where Hotkeys window isn't visible when ECCS_BLIND_MODE = n

--- agent.php
+++ agent.php
@@ -312,6 +312,7 @@ if(ECCS_BLIND_MODE != "y"){
                                 display: none;
                                 box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
                                 min-width: 480px;
+																																z-index: 1;
                         }
                         #popup-hotkeys .panel-heading {
                                 background-color: #2a2a2a;
