9715e4a8e2e59d7738323c74d21478f11e30911f
Chris <chris@iknowitworks.org>
2019-06-07 23:54:08 +0800

    Removed Live Helper Chat code

--- php/UIHandler.php
+++ php/UIHandler.php
@@ -1746,25 +1746,7 @@ error_reporting(E_ERROR | E_PARSE);
 			</div>
 			<!-- End of View Agent -->
 			<!-- End of modal -->
-   ';
-   
-   if (ENABLE_LHC) {
-    $footer .= '
-    <!-- Live Helper Chat -->
-    <script type="text/javascript">
-      var LHCChatOptions = {};
-      LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500};
-      (function() {
-      var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;
-      var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf("://")+1)) : "";
-      var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : "";
-      po.src = "//www.goautodial.com/livehelperchat-master/lhc_web/index.php/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true?r="+referrer+"&l="+location;
-      var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
-      })();
-    </script>
-    <!-- End of Live Helper Chat -->
-    ';
-   }
+                ';
 		return $footer;
 	}
 
