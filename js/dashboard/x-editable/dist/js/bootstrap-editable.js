3022710e7c93f1f6b0e914ed007621219537065b
Chris <chris@iknowitworks.org>
2020-05-05 02:46:32 +0800

    Fixing issue #8012

--- js/dashboard/x-editable/dist/js/bootstrap-editable.js
+++ js/dashboard/x-editable/dist/js/bootstrap-editable.js
@@ -1904,7 +1904,7 @@ Makes editable any HTML element on the page. Applied as jQuery method.
         @param {boolean} convertStr whether to convert value from string to internal format
         **/         
         setValue: function(value, convertStr, response) {
-            console.log(value);
+            console.log(this.container);
             if(convertStr) {
                 this.value = this.input.str2value(value);
             } else {
