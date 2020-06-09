10c40fcafbcaaa348f5071d952f1e5858741a67b
Chris <chris@iknowitworks.org>
2020-06-10 00:05:58 +0800

    Fixing ECCS callback issue

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -2627,12 +2627,6 @@ function hotKeysAvailable(e) {
                     alt_dial_active = 0;
                     alt_dial_status_display = 0;
                     DialedCallHangup('NO', 'YES', HKdispo);
-                    
-                    if (ECCS_BLIND_MODE == 'y' && ECCS_DIAL_TIMEOUT > 0 && HKdispo !== 'CALLBK') {
-                        setTimeout(function() {
-                            btnDialHangup();
-                        }, 1000);
-                    }
                 
                     if (custom_fields_enabled > 0) {
                         //vcFormIFrame.document.form_custom_fields.submit();
