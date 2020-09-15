b75149105f3e462b5ae33abf7dcaaf8f701a4d37
Chris <chris@iknowitworks.org>
2020-09-15 21:49:28 +0800

    Fixing the ECCS bug (again).

--- modules/GOagent/GOagentJS.php
+++ modules/GOagent/GOagentJS.php
@@ -2671,13 +2671,15 @@ function hotKeysAvailable(e) {
                     alt_dial_status_display = 0;
                     DialedCallHangup('NO', 'YES', HKdispo);
                     
-                    if (ECCS_BLIND_MODE == 'y' && ECCS_DIAL_TIMEOUT > 0 && dial_method !== "MANUAL") {
+                    if (ECCS_BLIND_MODE == 'y' && ECCS_DIAL_TIMEOUT > 0 && (dial_method !== "MANUAL" || (dial_method === "MANUAL" && HKdispo === 'CALLBK'))) {
                         if (HKdispo === 'CALLBK') {
                             is_call_cb = true;
                         }
-                        setTimeout(function() {
-                            btnDialHangup(true);
-                        }, (ECCS_DIAL_TIMEOUT * 1000));
+                        
+                        btnDialHangup(true);
+                        //setTimeout(function() {
+                        //    btnDialHangup(true);
+                        //}, (ECCS_DIAL_TIMEOUT * 1000));
                     }
                 
                     if (custom_fields_enabled > 0) {
