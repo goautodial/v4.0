e4194478ee5c710efcaa5ac9edcc777edcfc5ae1
Chris <chris@iknowitworks.org>
2021-01-04 21:33:06 +0800

    Fixing a bug on telephonyusers.php

--- telephonyusers.php
+++ telephonyusers.php
@@ -943,7 +943,7 @@
 							}											
 							
 						} else {
-							<?php echo $lh->translateText("no_data"); ?>
+							console.log("<?php echo $lh->translateText("no_data"); ?>");
 						}
 						
 					}
