b3a8fb248791214b20bb2f72e058b0277e44aed5
Thom Patacsil <patacsil.thom@gmail.com>
2021-03-09 16:56:48 -0500

    Rocketchat Embed and Login Functionality

---
+++ php/LoginRocketChat.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @file        LoginRocketChat.php
+ * @brief       Logs in user to rocketchat
+ * @copyright   Copyright (c) 2018 GOautodial Inc.
+ * @author      Alexander Jim Abenoja
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include('./CRMDefaults.php');
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+//Logs In Rocketchat User
+	$curl = curl_init();
+       	curl_setopt_array($curl, array(
+        CURLOPT_URL => ROCKETCHAT_URL."/api/v1/login",
+        CURLOPT_RETURNTRANSFER => true,
+       	CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+       	CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+       	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+       	CURLOPT_POSTFIELDS =>"{\r\n  \"user\": \"$user\",\r\n  \"password\": \"$pass\"}",
+        CURLOPT_HTTPHEADER => array(
+       	        "Content-Type:application/json"
+       	    )
+        ));
+       	$response = curl_exec($curl);
+        curl_close($curl);
+	echo $data = $response;
+	//echo json_encode($data);
+?>
