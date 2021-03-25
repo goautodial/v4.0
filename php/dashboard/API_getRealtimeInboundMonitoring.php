4b160924676d40fdb6a9739468746835dfae80ca
Chris <chris@iknowitworks.org>
2021-03-26 05:27:38 +0800

    For issue #8604

--- php/dashboard/API_getRealtimeInboundMonitoring.php
+++ php/dashboard/API_getRealtimeInboundMonitoring.php
@@ -155,8 +155,8 @@
             $closer_campaigns                       = implode(", ", explode(" ", $closer_campaigns));
             
             $calls_in_queue                         = 0;
-            if (is_array($output->calls_in_queue) && isset($output->calls_in_queue[$agentid])) {
-                $calls_in_queue = $output->calls_in_queue[$agentid];
+            if (is_array($output->calls_in_queue) && isset($output->calls_in_queue->{$agentid})) {
+                $calls_in_queue = $output->calls_in_queue->{$agentid};
             }
 			
 			switch ($status) {
