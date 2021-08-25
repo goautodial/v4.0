a9fb30503a86587466016a85206ae7673775d5e1
Chris <chris@iknowitworks.org>
2021-08-26 03:07:28 +0800

    Fixing the slow loading on issue #2719

--- telephonycampaigns.php
+++ telephonycampaigns.php
@@ -442,8 +442,7 @@
 												if($areacode->result == "success"){
 											   		for($i=0;$i < count($campaign->campaign_id);$i++){
 														$camp_id = $campaign->campaign_id[$i];
-														$areacode_arr = (array) $areacode;
-var_dump($areacode_arr);die();
+var_dump(json_decode($areacode, true));die();
 														if($areacode->active[$camp_id] == "Y"){
 															$areacode->active[$camp_id] = $lh->translationFor("active");
 														}else{
