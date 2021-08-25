ad7aea6a367d9242068a078fa784ff021941a3e0
Chris <chris@iknowitworks.org>
2021-08-26 02:49:48 +0800

    Fixing the slow loading on issue #2719

--- telephonycampaigns.php
+++ telephonycampaigns.php
@@ -440,24 +440,24 @@
 										   <tbody>
 											   <?php
 												if($areacode->result == "success"){
-													var_dump($areacode);
-											   		for($i=0;$i < count($areacode->campaign_id);$i++){
+											   		for($i=0;$i < count($campaign->campaign_id);$i++){
+														$camp_id = $campaign->campaign_id[$i];
 
-														if($areacode->active[$i] == "Y"){
-															$areacode->active[$i] = $lh->translationFor("active");
+														if($areacode->active[$camp_id] == "Y"){
+															$areacode->active[$camp_id] = $lh->translationFor("active");
 														}else{
-															$areacode->active[$i] = $lh->translationFor("inactive");
+															$areacode->active[$camp_id] = $lh->translationFor("inactive");
 														}
 
-													$action_CAMPAIGN = $ui->ActionMenuForAreacodes($areacode->areacode[$i], $areacode->campaign_id[$i], $perm);
+													$action_CAMPAIGN = $ui->ActionMenuForAreacodes($areacode->areacode[$camp_id], $areacode->campaign_id[$camp_id], $perm);
 											   ?>
 													<tr>
-														<td><?php if ($perm->campaign->campaign_update !== 'N') { echo '<a class="view_areacode" data-toggle="modal" data-target="#modal_edit_areacode" data-camp="'.$areacode->campaign_id[$i].'" data-ac="'.$areacode->areacode[$i].'">'; } ?><avatar username='<?php echo $areacode->campaign_name[$i];?>' :size='32'></avatar><?php if ($perm->campaign->campaign_update !== 'N') { echo '</a>'; } ?></td>
-														<td><strong><?php if ($perm->campaign->campaign_update !== 'N') { echo '<a class="view_areacode" data-toggle="modal" data-target="#modal_edit_areacode"  data-camp="'.$areacode->campaign_id[$i].'" data-ac="'.$areacode->areacode[$i].'">'; } ?><?php echo $areacode->campaign_id[$i];?><?php if ($perm->campaign->campaign_update !== 'N') { echo '</a>'; } ?></strong></td>
-														<td><?php echo $areacode->campaign_name[$i];?></td>
-														<td><?php echo $areacode->areacode[$i];?></td>
-														<td><?php echo $areacode->outbound_cid[$i];?></td>
-														<td><?php echo $areacode->active[$i];?></td>
+														<td><?php if ($perm->campaign->campaign_update !== 'N') { echo '<a class="view_areacode" data-toggle="modal" data-target="#modal_edit_areacode" data-camp="'.$areacode->campaign_id[$camp_id].'" data-ac="'.$areacode->areacode[$camp_id].'">'; } ?><avatar username='<?php echo $areacode->campaign_name[$camp_id];?>' :size='32'></avatar><?php if ($perm->campaign->campaign_update !== 'N') { echo '</a>'; } ?></td>
+														<td><strong><?php if ($perm->campaign->campaign_update !== 'N') { echo '<a class="view_areacode" data-toggle="modal" data-target="#modal_edit_areacode"  data-camp="'.$areacode->campaign_id[$camp_id].'" data-ac="'.$areacode->areacode[$camp_id].'">'; } ?><?php echo $areacode->campaign_id[$camp_id];?><?php if ($perm->campaign->campaign_update !== 'N') { echo '</a>'; } ?></strong></td>
+														<td><?php echo $areacode->campaign_name[$camp_id];?></td>
+														<td><?php echo $areacode->areacode[$camp_id];?></td>
+														<td><?php echo $areacode->outbound_cid[$camp_id];?></td>
+														<td><?php echo $areacode->active[$camp_id];?></td>
 														<td><?php echo $action_CAMPAIGN;?></td>													
 													</tr>
 											   <?php
