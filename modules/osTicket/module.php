9942d851209ea085916d76b0a990b8282689a546
Chris <chris@iknowitworks.org>
2022-03-25 03:20:29 +0800

    Updates for osTicket module

--- modules/osTicket/module.php
+++ modules/osTicket/module.php
@@ -75,7 +75,7 @@ class osTicket extends Module {
 		<script>
 					//Logging in on osTicket
 					$(function() {
-						$.post('{$osticket_url}', {username: '{$user}', passwd: '$pass', token: 't3M687em4$tad43u'}, function(result) {
+						$.post('{$osticket_url}', {username: '{$user}', passwd: '$pass', token: 'tpft4!tdK6nt7j6a'}, function(result) {
 							
 						});
 					});
